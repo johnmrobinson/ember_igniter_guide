ff4252c6ab5fff348c518d6f562ce1b004ad69d5
John Robinson <john.robinson@wellzesta.com>
2016-02-09 07:14:56 -0500

    touched add-todo.js

--- app/components/add-todo.js
+++ app/components/add-todo.js
@@ -8,8 +8,8 @@ export default Ember.Component.extend({
 	actions: {
 		submitMe() {
 			const _text = this.get('myText');
-			// myText is a property that is defined in the template. A template can inject a property.
-			// Alternatively, we can define a property as above. This gives us a default value.
+			// 'myText' is a property that is defined in the add-todo.hbs template. A property can be injected by the component's .js file or by the component's .hbs template file.
+			// My preference is to define all properties in the .js file, as above.
 			this.get('onAdd')(_text);
 			// onAdd is added as a property by the template. Turns out onAdd is a function.
 			// So, this.get('onAdd') gets the function action 'addItem' and calls the 'addItem' action (defined in todo-widget controller) with _text
